d754d19383e7f3a2b4e4767a30a3e9c9c6cd5843
nora <noraselenyi@gmail.com>
2019-06-28 14:50:33 +0200

    fix(HMMM-3): kingdom's name fixed

--- src/services/kingdom.service.js
+++ src/services/kingdom.service.js
@@ -2,7 +2,7 @@ const Kingdom = require('../models/kingdom.model');
 
 const createKingdom = (userId, username, kingdomName) => new Promise((resolve, reject) => {
   if (kingdomName.length < 1) {
-    kingdomName = username;
+    kingdomName = `${username}'s kingdom`;
   }
   const newKingdom = new Kingdom({ kingdomName, userId });
   newKingdom.save((err, data) => {
