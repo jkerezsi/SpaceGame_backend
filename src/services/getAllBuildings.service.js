03ecef30e73aff908c449686d88f357cd1c3cb4d
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-07-11 15:08:06 +0200

    feat(HMMM-23): Done controllers & services

--- src/services/getAllBuildings.service.js
+++ src/services/getAllBuildings.service.js
@@ -5,7 +5,11 @@ const requestAllBuildings = userIdp => new Promise((resolve, reject) => {
   Kingdom.findOne({ userId: userIdp }, '-_id -resources -userId -__v -kingdomName', (err, data) => {
     if (err) {
       reject(err);
-    } else {
+    }
+    else if (data==null){
+      reject(new Error('Could not find kingdom'))
+    }
+    else {
       resolve(data);
     }
   });
