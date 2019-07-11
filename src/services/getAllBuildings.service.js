2a98ca54ab655c52df7a0c8cbc02e4f960aaa1c2
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-07-11 16:11:18 +0200

    feat(HMMM-23): Refactor according to eslint

--- src/services/getAllBuildings.service.js
+++ src/services/getAllBuildings.service.js
@@ -5,11 +5,9 @@ const requestAllBuildings = userIdp => new Promise((resolve, reject) => {
   Kingdom.findOne({ userId: userIdp }, '-_id -resources -userId -__v -kingdomName', (err, data) => {
     if (err) {
       reject(err);
-    }
-    else if (data==null){
-      reject(new Error('Could not find kingdom'))
-    }
-    else {
+    } else if (data == null) {
+      reject(new Error('Could not find kingdom'));
+    } else {
       resolve(data);
     }
   });
