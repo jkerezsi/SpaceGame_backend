407f2fee9d02496a0f862bc5bc83ef3158bb8ac8
Hpeter1988 <hpeter1988@msn.com>
2019-07-16 13:42:09 +0200

    feat(HMMM-33) error message rewritten

--- src/services/getOneBulinding/filterOutOneBuiding.service.js
+++ src/services/getOneBulinding/filterOutOneBuiding.service.js
@@ -2,7 +2,7 @@
 
 const filterOutOneBuiding = (allbuidings, buildingId) => new Promise((resolve, reject) => {
   if (buildingId < 1) {
-    reject(new Error('Building id needed'));
+    reject(new Error('Building id needed!'));
   }
 
   const oneBuilding = allbuidings.buildings.filter(buidling => buidling._id.toString() === buildingId);
