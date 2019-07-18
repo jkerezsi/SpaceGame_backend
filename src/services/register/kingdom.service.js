3341eacfef83a440e0ad4fd7fb68ca467bec846d
Hpeter1988 <hpeter1988@msn.com>
2019-07-18 11:09:16 +0200

    feat(townhall) adding default townhall to kingdom

--- src/services/register/kingdom.service.js
+++ src/services/register/kingdom.service.js
@@ -1,15 +1,18 @@
 const Kingdom = require('../../models/kingdom.model');
 const Resources = require('../../models/resources.model');
+const Buildings = require('../../models/building.model');
 const { registerToken } = require('../../models/register-token');
+
 
 const createKingdom = (userId, username, kingdomName) => new Promise((resolve, reject) => {
   if (kingdomName === undefined) {
     kingdomName = `${username}'s kingdom`;
   }
-
+  const townhall = new Buildings({ type: 'townhall' });
   const goldResources = new Resources({ type: 'Gold' });
   const foodResources = new Resources({ type: 'Food' });
   const newKingdom = new Kingdom({ kingdomName, userId });
+  newKingdom.buildings.push(townhall);
   newKingdom.resources.push(foodResources, goldResources);
   newKingdom.save((err, data) => {
     if (err) {
