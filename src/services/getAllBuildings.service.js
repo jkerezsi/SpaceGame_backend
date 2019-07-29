5ceec7e82b869426d25ddf70a9a35c451c7a08a1
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-07-29 20:02:26 +0200

    feat(HMMM-23): working on Buildings Component

--- src/services/getAllBuildings.service.js
+++ src/services/getAllBuildings.service.js
@@ -2,13 +2,12 @@ const Kingdom = require('../models/kingdom.model');
 
 
 const requestAllBuildings = userIdp => new Promise((resolve, reject) => {
-  Kingdom.findOne({ userId: userIdp }, '-_id -resources -userId -__v -kingdomName', (err, data) => {
+  Kingdom.findOne({ userId: userIdp }, '-_id -resources -userId -__v -kingdomName', (err, usersstuff) => {
     if (err) {
-      reject(err);
-    } else if (data === null) {
       reject(new Error('Could not find kingdom'));
     } else {
-      resolve(data);
+      console.log(usersstuff)
+      resolve(usersstuff);
     }
   });
 });
