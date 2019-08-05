e0dbfc5f72428cae41d2d1acd47ccc9b743c4226
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-08-05 10:52:01 +0200

    fix(HMMM-23): eslint solving

--- src/services/getAllBuildings.service.js
+++ src/services/getAllBuildings.service.js
@@ -2,11 +2,11 @@ const Kingdom = require('../models/kingdom.model');
 
 
 const requestAllBuildings = userIdp => new Promise((resolve, reject) => {
-  Kingdom.findOne({ userId: userIdp }, '-_id buildings', (err, usersstuff) => {
+  Kingdom.findOne({ userId: userIdp }, '-_id buildings', (err, data) => {
     if (err) {
       reject(new Error('Could not find kingdom'));
     } else {
-      resolve(usersstuff);
+      resolve(data);
     }
   });
 });
