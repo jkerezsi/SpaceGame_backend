5dfd8f40aa8e9ed4bfa2fd34c825dcf4688abb0d
nora <noraselenyi@gmail.com>
2019-08-07 16:25:29 +0200

    hmmm38 test test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3012;
 
 require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+const { Mockgoose } = require('mockgoose');
+
+const mockgoose = new Mockgoose(mongoose);
 
 app.use(cors());
 app.use(express.json());
@@ -14,14 +19,33 @@ const router = require('./routes/index');
 
 app.use(router);
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-qmdil.mongodb.net/test?retryWrites=true`,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
+const connectToDatabase = async () => {
+  let databaseConnectionUri = `mongodb+srv://${process.env.DB_USERNAME}:${
+    process.env.DB_PASSWORD
+  }@cluster0-qmdil.mongodb.net/test?retryWrites=true`;
+
+  if (process.env.NODE_ENV === 'test') {
+    await mockgoose.prepareStorage();
+    databaseConnectionUri = 'mongodb://localhost/testDB';
+  }
+  try {
+    mongoose.connect(databaseConnectionUri, { useNewUrlParser: true });
+  } catch (error) {
+    console.log(error.message);
+  }
+  const database = mongoose.connection;
+  database.once('open', () => {
+    console.log(
+      `${
+        process.env.NODE_ENV === 'test' ? 'mock' : ''
+      } db successfully connected!`,
+    );
   });
+  return database;
+};
 
-app.listen(PORT, () => {
-  console.log(`Port is listening on ${PORT}`); // eslint-disable-line
-});
+const disconnectFromDatabase = async () => {
+  await mongoose.disconnect();
+};
 
-module.exports = app;
+module.exports = { app, connectToDatabase, disconnectFromDatabase };
--- test/integration-test/login-test.js
+++ test/integration-test/login-test.js
@@ -1,3 +1,64 @@
+/* eslint-disable no-unused-expressions */
+const app = require('../../src/index');
+const chai = require('chai');
+const request = require('supertest');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const { expect } = chai;
+
+describe('API Tests', () => {
+  it('/register success', (done) => {
+    request(app.app)
+      .post('/register')
+      .send({ username: 'New', password: 'morethan8' })
+      .end((err, res) => {
+        console.log(res);
+        
+        expect(res.status).to.equal(200);
+        done(err);
+      });
+  });
+
+  it('/register without password', (done) => {
+    request(app.app)
+      .post('/register')
+      .send({ username: 'New', password: '' })
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('error');
+        done(err);
+      });
+  });
+
+  it('/register with short password', (done) => {
+    request(app.app)
+      .post('/register')
+      .send({ username: 'New', password: 'n' })
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('error');
+        expect(res.body.message).to.equal('Password must be at least 8 characters.');
+        done(err);
+      });
+  });
+
+  // it('/login happenes', (done) => {
+  //   request(app.app)
+  //     .post('/login')
+  //     .set('Content-type', 'application/json')
+  //     .send({ username: 'muhahahah', password: '' })
+  //     .end((err, res) => {
+  //       // console.log(res.body)
+  //       expect(res.status).to.equal(400);
+  //       expect(res.text).to.equal('"Password is required."');
+  //       expect(res.body).to.equal('Password is required.');
+  //       done(err);
+  //     });
+  // });
+});
+
 // const app = require('./index');
 // const request = require('supertest');
 // const test = require('tape');
@@ -25,3 +86,5 @@
 //       t.end();
 //     });
 // });
+
+
